f0459818239daf4f6bd2651fd763321ab3fe912b
Busola Ogunbayo <Ifebusolaj95@gmail.com>
2023-10-05 11:36:35 -0700

    fixed my server now

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,10 +9,10 @@ sudo mkdir /data/web_static/releases/
 sudo mkdir /data/web_static/shared/
 sudo mkdir /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo '<h1>Hello World</h1>' > /data/web_static/releases/test/index.html
+echo '<h1>Hello World</h1>' > sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo chown ubuntu:ubuntu /data/
 
-sed -i '0,/=404;/s//=404;\n\t}\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;/' /etc/nginx/sites-available/default
+sudo sed -i '0,/=404;/s//=404;\n\t}\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;/' /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-enabled/
 sudo service nginx restart
