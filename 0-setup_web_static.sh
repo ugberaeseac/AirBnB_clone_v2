ab315b6765b3aced13a9691fc0fc3d31353f0114
Busola Ogunbayo <Ifebusolaj95@gmail.com>
2023-10-05 11:57:32 -0700

    switched the symlink creation for the indexfile

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,15 +3,14 @@
 
 sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-echo '<h1>Hello World</h1>' > sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
-sudo chown ubuntu:ubuntu /data/
+echo "Ifebusola's website" > sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current 
+sudo chown -R ubuntu:ubuntu /data/
 
 sudo sed -i '0,/=404;/s//=404;\n\t}\n\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current;/' /etc/nginx/sites-available/default
 sudo ln -sf /etc/nginx/sites-enabled/
