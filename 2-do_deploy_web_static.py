8bb0c9c86990bc059d7401e5d2e89480ca274f9a
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-08 15:59:53 -0700

    debugging: checking

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -28,9 +28,11 @@ def do_pack():
         return None
 
 
-""" def do_deploy(archive_path):
+def do_deploy(archive_path):
 
+    """
     deploy archive to web server
+    """
 
     if not os.path.exists(archive_path):
         return False
@@ -48,31 +50,6 @@ def do_pack():
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(filePath))
         print("New version deployed!")
-        return True
-    except Exception:
-        return False"""
-
-
-def do_deploy(archive_path):
-    """
-    Deploy archive to web servers
-    """
-
-    if os.path.isfile(archive_path) is False:
-        return False
-    try:
-        filename = archive_path.split("/")[-1]
-        no_ext = filename.split(".")[0]
-        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
-        symlink = "/data/web_static/current"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(path_no_ext))
-        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
-        run("rm -rf {}web_static".format(path_no_ext))
-        run("rm -rf {}".format(symlink))
-        run("ln -s {} {}".format(path_no_ext, symlink))
         return True
     except Exception:
         return False
