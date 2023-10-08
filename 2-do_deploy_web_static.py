4a71034c041c5c833ef3b87a3c61f735e5367661
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-08 15:28:57 -0700

    debugging

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 Fabric script to deploy archive to web servers
 Returns True if successful otherwise returns False
@@ -7,7 +8,24 @@ Returns True if successful otherwise returns False
 
 from fabric.api import *
 import os
+from time import strftime
 env.hosts = ['54.242.177.175', '52.91.148.117']
+
+
+def do_pack():
+    """
+    generate a .tgz archive
+    """
+    now = strftime("%Y%M%d%H%M%S")
+    local('mkdir -p versions/')
+    path = "versions/web_static_{}.tgz".format(now)
+    print("Generating archive...")
+    result = local('tar -zvcf {} web_static/'.format(path))
+
+    if result.succeeded:
+        return path
+    else:
+        return None
 
 
 def do_deploy(archive_path):
@@ -22,16 +40,15 @@ def do_deploy(archive_path):
         fileName = archive_path.split("/")[-1]
         noExtension = fileName.split(".")[0]
         filePath = "/data/web_static/releases/{}/".format(noExtension)
-        print("Deploying archive to webservers...")
         put(archive_path, "/tmp/")
         run("mkdir -p {}".format(filePath))
         run("tar -xzf /tmp/{} -C {}".format(fileName, filePath))
         run("rm /tmp/{}".format(fileName))
-        symLink = "/data/web_static/current"
         run("rm -rf {}".format(symLink))
         run("mv {}web_static/* {}".format(filePath, filePath))
         run("rm -rf {}web_static".format(filePath))
-        run("ln -sf {} {}".format(filePath, symLink))
+        run("ln -s {} /data/web_static/current".format(filePath))
+        print("New version deployed!")
         return True
     except Exception:
         return False
