e95bd83797e8c72c6b011436a51a4838ff8a95e2
UGBERAESE Amowie Charles <ugberaeseac@gmail.com>
2023-10-22 01:44:34 -0700

    script that starts a Flask web application, displays a custom text using a variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+web application listening on 0.0.0.0 and port 5000
+used strict_slashes=False in route definition
+Routes  /: display “Hello HBNB!”
+        /hbnb: display "HBNB"
+        /c/<text>: displays custom text, replaces '_' with ' '
+"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    """
+    display “Hello HBNB!”
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """
+    display "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_route(text):
+    """
+    displays custom text, replaces '_' with ' '
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
